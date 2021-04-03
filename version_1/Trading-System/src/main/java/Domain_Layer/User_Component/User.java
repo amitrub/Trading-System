eb9c4b01b11021d45a9dee3c5504722d61945a76
Amit <amitrub.github.com>
2021-04-03 20:14:59 +0300

    1

--- version_1/Trading-System/src/main/java/Domain_Layer/User_Component/User.java
+++ version_1/Trading-System/src/main/java/Domain_Layer/User_Component/User.java
@@ -10,6 +10,7 @@ public  class User {
     private static Trading_System trading_system;
 
     private Integer id;
+    private String password;
     private List<Integer> my_founded_storesiDs;
     private List<Integer> my_owned_storesiDs;
     private List<Integer> my_managed_storesiDs;
