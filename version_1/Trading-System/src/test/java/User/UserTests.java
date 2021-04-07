cc6baa7bf06ee9d1da269eda35d1dbc2072d3f14
elinorav <elinorav@post.bgu.ac.il>
2021-04-07 17:04:56 +0300

    #changes 2

--- version_1/Trading-System/src/test/java/User/UserTests.java
+++ version_1/Trading-System/src/test/java/User/UserTests.java
@@ -4,6 +4,7 @@ import TradingSystem.Client.Client;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.Assert.*;
 
 public class UserTests {
 
@@ -17,15 +18,57 @@ public class UserTests {
 
     @Test
     void registerHappy() {
-        int respondID = client.Register("Roee", "1234");
-//        todo: assert and all this sheet
+        int respondID1 = client.Register("Roee", "1234");
+        assertEquals(respondID1, 1);
+        int respondID2 = client.Register("Dani", "qwerty");
+        assertEquals(respondID2, 2);
     }
 
     @Test
-    void registerBad() {
+    void registerDuplicateUserName() {   //duplicate userName
+        int respondID1 = client.Register("Dani", "qwerty");
+        assertEquals(respondID1, 1);
+        int respondID2 = client.Register("Dani", "qqq");
+        assertEquals(respondID2, -1);
     }
 
     @Test
-    void registerSad() {
+    void registerPassword() {    //password is too short
+        int respondID1 = client.Register("Dani", "1");
+        assertEquals(respondID1, -1);
     }
+
+    @Test
+    void loginHappy(){
+        client.Register("Dani", "qwerty");
+        assertTrue(client.isLogin());
+        client.Logout();
+        int respondID = client.Login("Dani", "qwerty");
+        assertEquals(respondID, 1);
+    }
+
+    @Test
+    void loginIncorrectPassword(){
+        client.Register("Dani", "qwerty");
+        client.Logout();
+        int respondID = client.Login("Dani", "qwe");
+        assertEquals(respondID, -1);
+    }
+
+    @Test
+    void loginIncorrectUserName(){
+        client.Register("Dani", "qwerty");
+        client.Logout();
+        int respondID = client.Login("Roee", "qwerty");
+        assertEquals(respondID, -1);
+    }
+
+    @Test
+    void logoutHappy(){
+        client.Register("Dani", "qwerty");
+        client.Logout();
+        assertFalse(client.isLogin());
+    }
+
+
 }
