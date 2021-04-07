6297ca080a92bc5367a43c04c172848029fc0bee
reutlevy <48647948+reutlevy@users.noreply.github.com>
2021-04-07 13:38:11 +0300

    add log

--- version_1/Trading-System/src/main/java/TradingSystem/Server/DomainLayer/TradingSystemComponent/SearchSystem.java
+++ version_1/Trading-System/src/main/java/TradingSystem/Server/DomainLayer/TradingSystemComponent/SearchSystem.java
@@ -10,23 +10,23 @@ public class SearchSystem {
 
     private TradingSystem tradingSystem= TradingSystem.getInstance();
 
-    public List<DummyStore> SearchProductByName(String name){
-        List<DummyStore> dummyStores = new LinkedList<>();
-            for(Store store:tradingSystem.getStores().values()){
-                if(store.SearchByName(name)){
-                    dummyStores.add(new DummyStore(store.getId(),store.getName(),store.getFounderID(),store.getOwnersIDs(),store.getManagersIDs(),store.getDiscountPolicy(),store.getBuyingPolicy(),store.getRate(),store.getNumberOfRatings(),store.getShoppingHistory()));
-                }
-            }
-        return dummyStores;
-    }
-
-    public List<DummyStore> SearchProductByCategory(String category){
-        List<DummyStore> dummyStores = new LinkedList<>();
-        for(Store store:tradingSystem.getStores().values()){
-            if(store.SearchByCategory(category)){
-                dummyStores.add(new DummyStore(store.getId(),store.getName(),store.getFounderID(),store.getOwnersIDs(),store.getManagersIDs(),store.getDiscountPolicy(),store.getBuyingPolicy(),store.getRate(),store.getNumberOfRatings(),store.getShoppingHistory()));
-            }
-        }
-        return dummyStores;
-    }
+//    public List<DummyStore> SearchProductByName(String name){
+//        List<DummyStore> dummyStores = new LinkedList<>();
+//            for(Store store:tradingSystem.getStores().values()){
+//                if(store.SearchByName(name)){
+//                    dummyStores.add(new DummyStore(store.getId(),store.getName(),store.getFounderID(),store.getOwnersIDs(),store.getManagersIDs(),store.getDiscountPolicy(),store.getBuyingPolicy(),store.getRate(),store.getNumberOfRatings(),store.getShoppingHistory()));
+//                }
+//            }
+//        return dummyStores;
+//    }
+//
+//    public List<DummyStore> SearchProductByCategory(String category){
+//        List<DummyStore> dummyStores = new LinkedList<>();
+//        for(Store store:tradingSystem.getStores().values()){
+//            if(store.SearchByCategory(category)){
+//                dummyStores.add(new DummyStore(store.getId(),store.getName(),store.getFounderID(),store.getOwnersIDs(),store.getManagersIDs(),store.getDiscountPolicy(),store.getBuyingPolicy(),store.getRate(),store.getNumberOfRatings(),store.getShoppingHistory()));
+//            }
+//        }
+//        return dummyStores;
+//    }
 }
