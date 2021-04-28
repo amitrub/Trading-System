af09ab0836b3ca930c02fa0bd7c2c9ab681ef735
Hadas Zeira <zeirah@post.bgu.ac.il>
2021-04-28 10:26:56 +0300

    React

--- version_1/Trading-System/src/main/frontend/src/ApiClient.js
+++ version_1/Trading-System/src/main/frontend/src/ApiClient.js
@@ -1,9 +1,4 @@
 import axios from 'axios';
-import URLS from '../src/Configuration';
-
-// var ApiClient = {
-//     getTest: (path) => String;
-// }
 
 export const createApiClient = () => {
     //API CLIENT OBJECT
@@ -14,6 +9,27 @@ export const createApiClient = () => {
                 return res.data;
             });
         },
+
+        getConnectSystem: () => {   
+            return axios.get(`http://localhost:8080/api/home`).then((res) => {
+                console.log(res);
+                return res.data;
+            });
+        },
+
+        postRegister: (registerData, connID) => {
+            const headers = {
+                'Content-Type': 'application/json; utf-8',
+                'Accept': 'application/json',
+                'connID': connID
+              }
+
+            return axios.post(`http://localhost:8080/api/register`, 
+                            registerData, {headers: headers}).then((res) => {
+                console.log(res);
+                return res.data;
+            })
+        }
     }
 };
 
--- version_1/Trading-System/src/main/frontend/src/App.js
+++ version_1/Trading-System/src/main/frontend/src/App.js
@@ -1,41 +1,29 @@
-import React, {useState, useEffect} from "react";
+import React, {useState} from "react";
 import './App.css';
-import axios from "axios";
-import createApiClient from "../src/ApiClient"
-
-const api = createApiClient();
-
-const TestComponent = () => {
-
-    const [testState, setTestState] = useState([]);
-
-    const fetchTest = () => {
-      // const hello_msg = api.getTest();
-      // setTestState(hello_msg);
-      axios.get("http://localhost:8080/api/test").then(res => {
-          console.log(res);
-          const data = res.data;
-          setTestState(data);
-        });
-    }
-
-    useEffect(() => {
-      fetchTest();
-    }, []);
-
-    return <div>
-      <h1> Test Component </h1>
-      <p> information from api/test/: {testState} </p>
-    </div>
-}
-
+import TestComponent from './Components/TestComponent/TestComponent'
+import Register from './Components/Register/Register'
+import User from './Components/User/User'
 
 
 function App() {
+
+  const [currUserConnID, setCurrUserConnID] = useState('');
+  // const [error, setErrorState] = useState('');
+
+  function submitRegisterHandler(regData) {
+    console.log(regData);
+  }
+
+  function updateConnIDHandler(connID) {
+    setCurrUserConnID(connID)
+  }
+
   return (
     <div className="App">
-        ~ Trading System ~
+        <h1>~ Trading System ~</h1>
         <TestComponent/>
+        <User onUpdateConnID = {updateConnIDHandler}/>
+        <Register onSubmitRegister = {submitRegisterHandler} connID = {currUserConnID}/>
     </div>
   );
 }
