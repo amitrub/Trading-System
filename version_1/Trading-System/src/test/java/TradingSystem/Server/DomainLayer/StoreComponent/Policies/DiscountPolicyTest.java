b4844a013c9a80cee5ad74eb19fe03d35ec198d1
elinorav <elinorav@post.bgu.ac.il>
2021-04-30 14:10:03 +0300

    discount policy tests

---
+++ version_1/Trading-System/src/test/java/TradingSystem/Server/DomainLayer/StoreComponent/Policies/DiscountPolicyTest.java
@@ -0,0 +1,12 @@
+package TradingSystem.Server.DomainLayer.StoreComponent.Policies;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DiscountPolicyTest {
+    @Test
+    void name() {
+        
+    }
+}
