cf9959903780fcceb6d29fece98f6e60cef8b03e
elinorav <elinorav@post.bgu.ac.il>
2021-05-16 18:08:33 +0300

    Update parallel tests

--- version_1/Trading-System/src/test/java/TradingSystem/Server/DomainLayer/TradingSystemComponent/TradingSystemImplTest.java
+++ version_1/Trading-System/src/test/java/TradingSystem/Server/DomainLayer/TradingSystemComponent/TradingSystemImplTest.java
@@ -9,14 +9,12 @@ import TradingSystem.Server.DomainLayer.StoreComponent.Policies.Sales.Sale;
 import TradingSystem.Server.DomainLayer.StoreComponent.Product;
 import TradingSystem.Server.DomainLayer.StoreComponent.Store;
 import TradingSystem.Server.DomainLayer.TradingSystemComponent.Task.PurchaseTaskUnitTests;
+import TradingSystem.Server.DomainLayer.TradingSystemComponent.Task.RemoveProductTaskUnitTests;
 import TradingSystem.Server.DomainLayer.TradingSystemComponent.Task.ResultUnitTests;
 import TradingSystem.Server.DomainLayer.UserComponent.User;
 
 import java.util.*;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
 
 import TradingSystem.Server.ServiceLayer.DummyObject.DummyShoppingHistory;
 import TradingSystem.Server.ServiceLayer.DummyObject.Response;
@@ -424,6 +422,68 @@ class TradingSystemImplTest {
         Product product=new Product(4,"prod4","food",7.0,11);
         Response response= tradingSystemImpl.RemoveProduct(userID,storeid,product.getProductID(),connID);
         assertTrue(response.getIsErr());
+    }
+
+    @Test
+    void removeProductFromStoreWhileOtherClientBuyingItTest() {
+        List<boolean[]> isErrsTotal = new ArrayList<>();
+        for (int test_i = 0; test_i < 10; test_i++) {
+            //Prepare
+            tradingSystemImpl.AddProductToStore(ElinorID, EconnID, ElinorStore, "Sneakers", "Shoes", 150.0, 25);
+            Integer newProduct = tradingSystemImpl.stores.get(ElinorStore).getProductID("Sneakers");
+            //Create two clients with task to buy this product
+            ExecutorService executor = (ExecutorService) Executors.newFixedThreadPool(2);
+
+            //Prepare tasks for clients
+            List<Callable<ResultUnitTests>> taskList = new ArrayList<>();
+            Callable<ResultUnitTests> purchaseTask = new PurchaseTaskUnitTests("guestBuyer", ElinorStore, newProduct, 25, "123456", "052897878787", "sioot st. 5");
+            taskList.add(purchaseTask);
+            Callable<ResultUnitTests> removeTask = new RemoveProductTaskUnitTests("Client-StoreOwner",ElinorStore, newProduct);
+            taskList.add(removeTask);
+
+            //Execute all tasks and get reference to Future objects
+            List<Future<ResultUnitTests>> resultList = null;
+
+            try {
+                resultList = executor.invokeAll(taskList);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            executor.shutdown();
+
+            System.out.println("\n========Printing the results======");
+            boolean[] isErrs = new boolean[2];
+            for (int i = 0; i < resultList.size(); i++) {
+                Future<ResultUnitTests> future = resultList.get(i);
+                try {
+                    ResultUnitTests result = future.get();
+//                System.out.println(result.getName() + ": " + result.getTimestamp());
+                    Response response = result.getResponse();
+                    System.out.println("Assert correctnes for " + result.getName() + ": response -> " + response + " ::" + result.getTimestamp());
+                    isErrs[i] = response.getIsErr();
+                } catch (InterruptedException | ExecutionException e) {
+                    e.printStackTrace();
+                }
+            }
+            //Check that one of the client failed and the other succeed.
+//            assertTrue((isErrs[0] && !isErrs[1]) || (isErrs[1] && !isErrs[0]));
+            isErrsTotal.add(isErrs);
+            tearDown();
+            setup();
+        }
+        boolean ans = false;
+        for(boolean[] errArr : isErrsTotal) {
+            if ((errArr[0] && !errArr[1]) || (errArr[1] && !errArr[0])) {
+                ans = true;
+                break;
+            }
+        }
+        assertTrue(ans);
+        System.out.println("========Printing the results - TOTAL PARALLEL ======");
+        for(int i=0; i<isErrsTotal.size(); i++) {
+            System.out.printf("%d: purchase: %s remove: %s\n", i, isErrsTotal.get(i)[0], isErrsTotal.get(i)[1]);
+        }
     }
 
     // requirement 4.2
