f20958dcc473bdaab4afccb2f00e55fc58912c15
Hadas Zeira <zeirah@post.bgu.ac.il>
2021-06-13 16:42:23 +0300

    GUI - Update Programres/Recos

--- version_1/Trading-System/src/main/frontend/src/Components/OtherComponents/MainPage/Recommendations.js
+++ version_1/Trading-System/src/main/frontend/src/Components/OtherComponents/MainPage/Recommendations.js
@@ -1,7 +1,6 @@
 import "../../../Design/grid.css";
 import "../../../Design/style.css";
-import React, { Component }  from 'react';
-
+import React, { Component } from "react";
 
 function Recommendations() {
   return (
@@ -12,27 +11,27 @@ function Recommendations() {
       <div className="row">
         <div className="col span-1-of-3">
           <blockquote>
-            Omnifood is just awesome! I just launched a startup which leaves me
-            with no time for cooking, so Omnifood is a life-saver. Now that I
-            got used to it, I couldn't live without my daily meals!
+            AliBamba is just awesome! I just ordered a birthday gift for my
+            nepuw, and I recieved it in 3 days!! AliBamba is a life-saver. Now
+            that I got used to it, I couldn't live without my daily orders!
             <img alt="" src="resources/img/customer-1.jpg" />
             Alberto Duncan
           </blockquote>
         </div>
         <div className="col span-1-of-3">
           <blockquote>
-            Inexpensive, healthy and great-tasting meals, delivered right to my
-            home. We have lots of food delivery here in Lisbon, but no one comes
-            even close to Omifood. Me and my family are so in love!
+            Inexpensive, big stock and great prices, delivered right to my home.
+            We have lots of shops here in Tel Aviv, but no one comes even close
+            to AliBamba. Me and my family are so in love!
             <img alt="" src="resources/img/customer-2.jpg" />
             Joana Silva
           </blockquote>
         </div>
         <div className="col span-1-of-3">
           <blockquote>
-            I was looking for a quick and easy food delivery service in San
-            Franciso. I tried a lot of them and ended up with Omnifood. Best
-            food delivery service in the Bay Area. Keep up the great work!
+            I was looking for a quick and easy iphone case in Ramat Gan. I tried
+            a lot of them and ended up with AliBamba. Best price I found from
+            all stores and webs. Keep up the great work!
             <img alt="" src="resources/img/customer-3.jpg" />
             Milton Chapman
           </blockquote>
